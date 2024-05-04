6369eb20fe0091ba96d3ebaed605a182f476626c
Skynight666 <161405183+Skynight666@users.noreply.github.com>
2024-05-05 01:50:40 +0800

    Update and rename JP308.java to JPA308.java

--- JPA308.java
+++ JPA308.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JP308 {
+public class JPA308 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
