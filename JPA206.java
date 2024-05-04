4e56957531652f87f7954e0aa37876b5daf916c8
Skynight666 <161405183+Skynight666@users.noreply.github.com>
2024-05-05 01:47:33 +0800

    Update and rename JP2.java to JPA206.java

--- JPA206.java
+++ JPA206.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JP2 {
+public class JPA206 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n1 = scn.nextInt();
