d90304a60e740de77be955fcc16314cb1ff6dfa5
Skynight666 <161405183+Skynight666@users.noreply.github.com>
2024-05-05 01:51:30 +0800

    Update and rename JP301.java to JPA302.java

--- JPA302.java
+++ JPA302.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JP301 {
+public class JPA302 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
